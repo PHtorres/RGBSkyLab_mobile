5b352768ab88fa2186d42ece15e7fb6dabc88bbc
Paulo Henrique Torres <paulohenriquetorresdev@gmail.com>
2020-08-08 00:22:13 -0300

    criando model e serviço para obter perfil cliente

---
+++ src/models/IPerfilCliente.ts
@@ -0,0 +1,8 @@
+interface IPerfilCliente {
+    apelido: string;
+    numeroUsuarios: number;
+    faturaRisco: boolean;
+    listaErros: string[];
+}
+
+export default IPerfilCliente;
---
+++ src/services/ServicoPerfilCliente.ts
@@ -0,0 +1,23 @@
+import IPerfilCliente from '../models/IPerfilCliente';
+import API from '../services/API';
+
+export default class ServicoPerfilCliente{
+
+    private api:API;
+
+    constructor(){
+        this.api = new API();
+    }
+
+    public async ObterPerfilCliente(apelidoCliente:string):Promise<IPerfilCliente>{
+
+        const resposta = await this.api
+        .FazerGet<IPerfilCliente>(`Contato/ObterContato/${apelidoCliente}`, '');
+
+        if(resposta){
+            return resposta;
+        }
+
+        return {listaErros:['Não foi possível obter o perfil do cliente']} as IPerfilCliente;
+    }
+}
