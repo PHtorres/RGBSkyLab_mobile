4d5aca63e997f0c54c0bcd4ca2c3526007f2608a
Paulo Henrique Torres <paulohenriquetorresdev@gmail.com>
2020-08-16 03:11:55 -0300

    criando splash screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   View,
   Text,
   StatusBar,
 } from 'react-native';
+
+import SplashScreen from 'react-native-splash-screen'
 
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -14,6 +16,9 @@ import Rotas from './src/routes/';
 
 
 function App() {
+
+  useEffect(() => { SplashScreen.hide() }, []);
+
   return (
     <NavigationContainer>
       <StatusBar barStyle="dark-content" backgroundColor="#7159c1" />
